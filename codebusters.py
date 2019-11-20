d4cd48f1fc52d6aaaca82dbce388ddeac5f5ce44
muchaco <adam_meszaros@fastmail.com>
2019-11-20 10:28:56 +0100

    codebusters

--- codebusters.py
+++ codebusters.py
@@ -6,6 +6,7 @@ from random import randint
 
 MAP_HEIGHT = 9000
 MAP_WIDTH = 16000
+MAX_DISTANCE = 20000
 
 GHOST_MAX_RANGE = 1760
 GHOST_MIN_RANGE = 900
@@ -18,107 +19,128 @@ BUSTER_COUNT = int(input())  # the amount of busters you control
 GHOST_COUNT = int(input())  # the amount of ghosts on the map
 TEAM_ID = int(input())  # if this is 0, your base is on the top left of the map, if it is one, on the bottom right
 
+DESTINATION = {0: (0,0),
+               1: (MAP_WIDTH, MAP_HEIGHT)}
+
+
 class C(object):
     def __init__(self, x, y):
-        self.x = x
-        self.y = y
+        self._x = x
+        self._y = y
 
-    def get(self):
-        return (self.x, self.y)
+    @property
+    def y(self):
+        return int(self._y)
 
-    def direction(self, p):
-        x, y = p.x, p.y
-        return C(x-self.x, y-self.y)
+    @property
+    def x(self):
+        return int(self._x)
 
-    def transpose(self):
-        return C(MAP_WIDTH - self.x, MAP_HEIGHT - self.y)
+    def __str__(self):
+        return '<' + str(self.x) + ',' + str(self.y) + '>'
 
-    def distance(self, p):
-        p0 = self.direction(p)
-        return p0.abs()
+    def __eq__(self, other):
+        return self.y == other.y and self.x == other.x
+
+    def __repr__(self):
+        return "C(%d, %d)" % (self.x, self.y)
+
+    def dist(self, other):
+        return (other-self).abs()
+
+    def __add__(self, other):
+        return C(self._x+other._x, self._y+other._y)
+
+    def __sub__(self, other):
+        return C(self._x-other._x, self._y-other._y)
 
     def abs(self):
-        return math.sqrt(self.x**2+self.y**2)
+        return math.sqrt(self._x**2+self._y**2)
 
-    def mul(self, i):
-        return C(self.x*i, self.y*i)
+    def __mul__(self, const):
+        return C(self._x*const, self._y*const)
 
-    @staticmethod
-    def average(list_of_coords):
-        x, y = 0, 0
-        for coord in list_of_coords:
-            x += coord.x
-            y += coord.y
+    def __truediv__(self, const):
+        return C(self._x/const, self._y/const)
 
-        return C(int(x/len(list_of_coords)), int(y/len(list_of_coords)))
+    # special methods
 
     def is_close_to_wall(self):
-        if FOG_DIST < self.x < MAP_WIDTH-FOG_DIST and \
-            FOG_DIST < self.y < MAP_HEIGHT-FOG_DIST:
+        if FOG_DIST < self._x < MAP_WIDTH-FOG_DIST and \
+            FOG_DIST < self._y < MAP_HEIGHT-FOG_DIST:
             return False
 
         return True
 
+    @staticmethod
+    def random(max_x, max_y):
+        return C(randint(0, max_x),
+                 randint(0, max_y))
 
-def random_coord(coord):
+
+def random_coord(reference):
     while True:
-        c = C(randint(0, MAP_WIDTH),randint(0, MAP_HEIGHT))
-        #print_debug(old_c = coord.get(), new_c = c.get())
-        if c.distance(coord) > FOG_DIST*2 and c.is_close_to_wall():
-            return c
-
-
-DESTINATION = {0: C(0,0),
-               1: C(MAP_WIDTH, MAP_HEIGHT)}
+        coords = C.random(MAP_WIDTH, MAP_HEIGHT)
+        if coords.dist(reference) > FOG_DIST*2 and coords.is_close_to_wall():
+            return coords
 
 
 class Entity(object):
+    instances = []
+
+    def __init__(self, _id, x, y, _type, state, value):
+        entity = self.__class__.get(_id)
+        if entity is None:
+            super(Entity, self).__init__(_id, x, y, _type, state, value)
+            self.__class__.instances.append(self)
+            self.initialize()
+        else:
+            entity.update(_id, x, y, _type, state, value)
+
     def __init__(self, _id, x, y, _type, state, value):
         self.update(_id, x, y, _type, state, value)
 
     def update(self, _id, x, y, _type, state, value):
-        self.id = _id
+        self._id = _id
         self.coords = C(x, y)
         self.type = _type
         self.state = state
         self.value = value
 
-    def distance(self, entity):
-        return self.coords.distance(entity.coords)
+    def initialize(self):
+        raise NotImplementedError
 
-    def get_id(self):
-        return self.id
+    def dist(self, entity):
+        if isinstance(entity, C):
+            return self.coords.dist(entity)
+        else:
+            return self.coords.dist(entity.coords)
+
+    @classmethod
+    def get(cls, _id):
+        for entity in __class__.instances:
+            if entity.id == _id:
+                return entity
+        else:
+            return None
+
+    @property
+    def id(self):
+        return self._id
 
 
 class Buster(Entity):
-    instances = []
-
-    def __init__(self, _id, x, y, _type, state, value):
-        buster = self.__class__.get_seen_one(_id)
-        if buster is None:
-            super(Buster, self).__init__(_id, x, y, _type, state, value)
-            self.__class__.instances.append(self)
-            self.next_coords = None
-            self.next_action = None
-            self.to_be_busted = None
-            self.random = None
-        else:
-            buster.update(_id, x, y, _type, state, value)
+    def initialize(self):
+        self.next_coords = None
+        self.next_action = None
+        self.to_be_busted = None
+        self.random = None
 
     def update(self, _id, x, y, _type, state, value):
         super(Buster, self).update(_id, x, y, _type, state, value)
-        if self.is_carrying():
-            ghost = Ghost.get_seen_one(self.carried_ghost_id())
-            if ghost is not None:
-                ghost.set_busted()
-
-    @classmethod
-    def get_seen_one(cls, _id):
-        for buster in __class__.instances:
-            if buster.get_id() == _id:
-                return buster
-        else:
-            return None
+        ghost = self.carried_ghost()
+        if ghost is not None:
+            ghost.set_busted()
 
     def is_enemy(self):
         return self.type != TEAM_ID
@@ -126,9 +148,9 @@ class Buster(Entity):
     def is_carrying(self):
         return self.state == 1
 
-    def carried_ghost_id(self):
+    def carried_ghost(self):
         if self.is_carrying():
-            return self.value
+            return Ghost.get(self.value)
         else:
             return None
 
@@ -154,7 +176,7 @@ class Buster(Entity):
 
     def get_next_action(self):
         if self.next_action == 'MOVE':
-            return ' '.join([self.next_action, *[str(c) for c in self.next_coords.get()]])
+            return ' '.join([self.next_action, str(self.next_coords.x, self.next_coords.y)])
         if self.next_action == 'BUST':
             busting = self.to_be_busted
             self.to_be_busted = None
@@ -162,43 +184,28 @@ class Buster(Entity):
         return 'RELEASE'
 
     @classmethod
-    def my_sorted_busters(cls):
+    def my_busters(cls):
         return sorted([b for b in cls.instances if not b.is_enemy()], key=lambda i: i.id)
 
-    @classmethod
-    def enemy_sorted_busters(cls):
-        return sorted([b for b in cls.instances if b.is_enemy()], key=lambda i: i.id)
+    # @classmethod
+    # def enemy_busters(cls):
+    #     return sorted([b for b in cls.instances if b.is_enemy()], key=lambda i: i.id)
 
 
 class Ghost(Entity):
-    instances = []
+    def initialize(self):
+        self.busted = False
 
-    def __init__(self, _id, x, y, _type, state, value):
-        ghost = self.__class__.get_seen_one(_id)
-        if ghost is None:
-            super(Ghost, self).__init__(_id, x, y, _type, state, value)
-            self.__class__.instances.append(self)
-            self.busted = False
-        else:
-            ghost.update(_id, x, y, _type, state, value)
-            ghost.busted=False
+    def update(self, _id, x, y, _type, state, value):
+        super(Ghost, self).update(_id, x, y, _type, state, value)
+        self.initialize()
 
-    @classmethod
-    def get_seen_one(cls, _id):
-        for ghost in __class__.instances:
-            if ghost.get_id() == _id:
-                return ghost
-        else:
-            return None
-
-    def trapping_busters(self):
-        return self.value
+    # @property
+    # def trapping_busters(self):
+    #     return self.value
 
     def set_busted(self):
         self.busted = True
-
-    def is_busted(self):
-        return self.busted
 
     def is_free(self):
         return not self.busted
@@ -208,7 +215,7 @@ class Ghost(Entity):
         return [ghost for ghost in cls.instances if ghost.is_free() and not ghost.is_targeted()]
 
     def is_targeted(self):
-        for b in Buster.my_sorted_busters():
+        for b in Buster.my_busters():
             if self.id == b.to_be_busted:
                 return True
         else:
@@ -216,10 +223,10 @@ class Ghost(Entity):
 
     def closest_buster(self):
         _id = -1
-        dist = 10000000000
+        dist = MAX_DISTANCE
 
-        for b in Buster.my_sorted_busters():
-            d = self.coords.distance(b.coords)
+        for b in Buster.my_busters():
+            d = self.dist(b)
             if d < dist:
                 dist = d
                 _id = b.id
@@ -227,15 +234,15 @@ class Ghost(Entity):
         if _id == -1:
             return None
 
-        return Buster.get_seen_one(_id)
+        return Buster.get(_id)
 
     @classmethod
-    def closest_ghost(cls, c):
+    def closest_ghost(cls, buster):
         _id = -1
-        dist = 10000000000
+        dist = MAX_DISTANCE
 
         for ghost in cls.get_free_ghosts():
-            d = ghost.coords.distance(c)
+            d = buster.dist(ghost)
             if d < dist:
                 dist = d
                 _id = ghost.id
@@ -243,50 +250,42 @@ class Ghost(Entity):
         if _id == -1:
             return None
 
-        return cls.get_seen_one(_id)
+        return cls.get(_id)
 
 
 def print_debug(**d):
     print(pprint.pformat(d), file=sys.stderr)
 
 
+def entity_initializer(*args):
+    entity_type = Ghost if args[3] < 0 else Buster
+    entity_type(*args)
+
+
 while True:
-    entities = int(input())  # the number of busters and ghosts visible to you
-    ghosts = []
-    busters = []
+    num_of_entities = int(input())  # the number of busters and ghosts visible to you
+    for i in range(num_of_entities):
+        entity_initializer([int(j) for j in input().split()])
 
-    for i in range(entities):
-        entity_id, x, y, entity_type, state, value = [int(j) for j in input().split()]
-
-        if entity_type < 0:
-            ghosts.append(Ghost(entity_id, x, y, entity_type, state, value))
-        else:
-            busters.append(Buster(entity_id, x, y, entity_type, state, value))
-
-    #print_debug(busters=[buster.id for buster in Buster.my_sorted_busters()])
-
-    for buster in Buster.my_sorted_busters():
-
+    for buster in Buster.my_busters():
         random = buster.get_random()
-        closest_ghost = Ghost.closest_ghost(buster.coords)
+        closest_ghost = Ghost.closest_ghost(buster)
 
-        if buster.carried_ghost_id() is not None and DESTINATION[TEAM_ID].distance(buster.coords) < RELEASE_RANGE:
+        if buster.carried_ghost() is not None and buster.dist(C(*DESTINATION[TEAM_ID])) < RELEASE_RANGE:
             buster.set_next_action('RELEASE')
-        elif buster.carried_ghost_id() is not None:
-            buster.set_next_coords(DESTINATION[TEAM_ID])
+        elif buster.carried_ghost() is not None:
+            buster.set_next_coords(C(*DESTINATION[TEAM_ID]))
         elif closest_ghost is not None: # and closest_ghost.closest_buster() == buster.id:
             print_debug(closest_ghost=closest_ghost.id, my_id=buster.id, closest_buster=closest_ghost.closest_buster().id)
-            ghost = Ghost.closest_ghost(buster.coords)
-            if GHOST_MIN_RANGE < ghost.coords.distance(buster.coords) < GHOST_MAX_RANGE:
-                buster.set_to_be_busted(ghost.id)
+            if GHOST_MIN_RANGE < closest_ghost.dist(buster) < GHOST_MAX_RANGE:
+                buster.set_to_be_busted(closest_ghost.id)
             else:
-                buster.set_next_coords(ghost.coords)
+                buster.set_next_coords(closest_ghost.coords)
         else:
             if random is None:
-                #print_debug(random=True)
                 buster.set_random()
             else:
-                if buster.next_coords.distance(buster.coords) < FOG_DIST:
+                if buster.dist(buster.next_coords) < FOG_DIST:
                     buster.set_random()
                 else:
                     buster.random = buster.next_coords
